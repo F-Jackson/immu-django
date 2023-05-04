70715e66ea7e06e54c91d487c828881892c52c6e
Home1998 <jacksonfinal@gmail.com>
2023-05-03 21:18:15 -0300

    making saving for the model

--- app/models.py
+++ app/models.py
@@ -1,33 +1,54 @@
+from datetime import timedelta
+import json
 from django.db import models
-
+from django.utils.timezone import now
 from abc import ABC
+from django.utils.crypto import get_random_string
+import random
 
 from immudb_connection.connection import starting_db
 
 
 immu_client = starting_db(user='immudb', password='immudb')
-print(immu_client)
+
+def random_uuid():
+    random_str = get_random_string(length=random.randint(10, 255))
+    immu_obj = immu_client.get(random_str.encode())
+    
+    if immu_obj is not None:
+        return random_uuid()
+
+    return random_str
 
 class ImmudbModel(models.Model):
+    uuid = models.CharField(max_length=255, default=random_uuid())
     nome = models.CharField(max_length=155)
-    # def save(self, *args, **kwargs):
-    #     values = {}
-    #     for field in self.__class__._meta.fields:
-    #         value = getattr(self, field.name)
-    #         values[field.name] = str(value)
+    ok = models.IntegerField()
+    
+    immu_confs = {
+        'expireableDateTime': None
+    }
+    
+    def save(self, *args, **kwargs):
+        values = {}
+        for field in self.__class__._meta.fields:
+            if field.name is not 'immu_confs' and field.name != 'id':
+                value = getattr(self, field.name)
+                values[field.name] = str(value)
 
-    #     if self.expireableDateTime:
-    #         self.immu_client.expireableSet(
-    #             self.pk.encode(),
-    #             json.dumps(values).encode(),
-    #             datetime.now() + timedelta(**self.expireableDateTime)
-    #         )
-    #     else:
-    #         self.immu_client.set(
-    #             self.pk.encode(),
-    #             json.dumps(values).encode()
-    #         )
-    #     super().save(*args, **kwargs)
+        print(values)
+        if self.immu_confs['expireableDateTime'] is not None:
+            immu_client.expireableSet(
+                self.uuid.encode(),
+                json.dumps(values).encode(),
+                now() + timedelta(**self.immu_confs['expireableDateTime'])
+            )
+        else:
+            immu_client.set(
+                self.uuid.encode(),
+                json.dumps(values).encode()
+            )
+        # super().save(*args, **kwargs)
 
     # def delete(self):
     #     deleteRequest = DeleteKeysRequest(keys=[self.pk.encode()])
